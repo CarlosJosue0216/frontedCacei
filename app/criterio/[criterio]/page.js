820b8d8e6f645dc4f1310f94d7a223f2a0c70c1c
ISC-2 <carlosjosue0216>
2024-02-27 11:01:18 -0600

    añadir input file

--- app/criterio/[criterio]/page.js
+++ app/criterio/[criterio]/page.js
@@ -40,26 +40,36 @@ export default function Page({ params }) {
       ) : (
         show &&
         Array.isArray(preguntasFiltradas) && (
-            <form className="w-full mx-auto p-4 border rounded shadow-lg">
+          <form className="w-3/4  mx-auto p-4 border rounded shadow-lg flex flex-col items-center justify-center">
             {preguntasFiltradas.map((pregunta) => (
-              <div key={pregunta.id} className="mb-4 p-5 text-center">
-                <h2 className="text-lg font-semibold mb-2">{pregunta.criterio + " .-" + pregunta.titulo}</h2>
-                <div className="flex justify-center items-center ">
-   
-                {pregunta.respuestas.map((respuesta) => (
-                  <div key={respuesta.id} className="mx-2 p-3">
-                    <input
-                      type="radio"
-                      id={`respuesta_${respuesta.id}`}
-                      name={`pregunta_${pregunta.id}`}
-                      value={respuesta.contenido}
-                      className="mx-2"
-                      />
-                    <label htmlFor={`respuesta_${respuesta.id}`} className="cursor-pointer">
-                      {respuesta.contenido}
-                    </label>
+              <div key={pregunta.id} className="mb-4 p-5 text-center flex items-center justify-center w-full ">
+                <div className="w-3/4 p-3 border-r-2">
+                  <h2 className="text-lg font-semibold mb-2">
+                    {pregunta.criterio + " .-" + pregunta.titulo}
+                  </h2>
+                  <div className="flex justify-center items-center ">
+                    {pregunta.respuestas.map((respuesta) => (
+                      <div key={respuesta.id} className="flex items-center mx-2 p-2">
+                        <input
+                          type="radio"
+                          id={`respuesta_${respuesta.id}`}
+                          name={`pregunta_${pregunta.id}`}
+                          value={respuesta.contenido}
+                          className="mx-2"
+                        />
+                        <label
+                          htmlFor={`respuesta_${respuesta.id}`}
+                          className="cursor-pointer"
+                        >
+                          {respuesta.contenido}
+                        </label>
+                      </div>
+                    ))}
                   </div>
-                ))}
+                </div>
+                <div className="w-1/4 flex flex-col p-3">
+                  <label for="myfile">Subir evidencia:</label>
+                  <input type="file" id="myfile" name="myfile" />
                 </div>
               </div>
             ))}
@@ -67,7 +77,7 @@ export default function Page({ params }) {
               type="submit"
               className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
             >
-              Enviar respuestas
+              Guardar respuestas
             </button>
           </form>
         )
--- app/page.js
+++ app/page.js
@@ -2,42 +2,175 @@
 import Link from "next/link";
 import PagePreguntas from "./components/preguntas";
 import { Auth } from "./middlewares/auth";
+import { useState } from "react";
 
 export default function Home() {
+  const [criterio1, setCriterio1] = useState(false);
+  const handleCriterio = () => {
+    setCriterio1(!criterio1);
+  };
+
   Auth();
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
-      <div className="p-5">
-        <h1 className="text-5xl font-black">
-          Criterios e indicadores para programas de ingenieria
-        </h1>
+    <main className="flex min-h-screen w-full flex-col items-center bg-gray-100 p-24">
+      <h1 className="text-5xl font-black p-5 my-5">
+        Criterios e indicadores para programas de ingenieria
+      </h1>
+      <div className="p-5 bg-white rounded-lg shadow-md my-5 ">
         <div className="p-5">
-          <h1>Criterio 1.- Personal academico</h1>
-          <div>
-            <Link href={"/criterio/1.1"}>
-              Criterio 1.1.- Perfil del personal académico
-            </Link>
-          </div>
-          <div>
-            <Link href={"/criterio/1.2"}>
-              Criterio 1.2.- Suficiencia de la planta académica
-            </Link>
-          </div>
-          <div>
-          <Link href={"/criterio/1.3"}>Criterio 1.3.- Distribución de las actividades sustantivas </Link> 
-          
-          </div>
-          <div>
-          <Link href={"/criterio/1.4"}>Criterio 1.4.- Evaluación y desarrollo del personal académico</Link>
-          </div>
-          <div>
-           
-          <Link href={"/criterio/1.5"}>Criterio 1.5.- Responsabilidad del personal académico con el plan de estudio</Link> 
-          </div>
-          <div>
-          
-          <Link href={"/criterio/1.6"}>Criterio 1.6.- Selección, permanencia y retencion del personal académico </Link> 
-          </div>
+          <h1
+            className="font-bold text-2xl hover:cursor-pointer flex items-center gap-10"
+            onClick={handleCriterio}
+          >
+            Criterio 1.- Personal academico
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m8 7 4 4 4-4m-8 6 4 4 4-4"
+              />
+            </svg>
+          </h1>
+          {criterio1 && (
+            <div>
+              <div className="p-2">
+                <Link
+                  className="font-semibold ms-3 flex justify-between"
+                  href={"/criterio/1.1"}
+                >
+                  Criterio 1.1.- Perfil del personal académico
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white mx-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link
+                  className="font-semibold ms-3 justify-between flex"
+                  href={"/criterio/1.2"}
+                >
+                  Criterio 1.2.- Suficiencia de la planta académica
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white mx-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link
+                  className="font-semibold ms-3 justify-between flex"
+                  href={"/criterio/1.3"}
+                >
+                  Criterio 1.3.- Distribución de las actividades sustantivas{" "}
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white mx-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link
+                  className="font-semibold ms-3 justify-between flex"
+                  href={"/criterio/1.4"}
+                >
+                  Criterio 1.4.- Evaluación y desarrollo del personal académico
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white mx-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link
+                  className="font-semibold ms-3 justify-between flex"
+                  href={"/criterio/1.5"}
+                >
+                  Criterio 1.5.- Responsabilidad del personal académico con el
+                  plan de estudio
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white mx-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </div>
+              <div className="p-2">
+                <Link
+                  className="font-semibold ms-3 justify-between flex"
+                  href={"/criterio/1.6"}
+                >
+                  Criterio 1.6.- Selección, permanencia y retencion del personal
+                  académico{" "}
+                  <svg
+                    className="w-6 h-6 text-gray-800 dark:text-white mx-3"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </div>
+            </div>
+          )}
         </div>
         <div className="p-5">
           <h1>Criterio 2.- Estudiantes</h1>
