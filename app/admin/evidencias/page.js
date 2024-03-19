8a3aa10a586637350ed99f493f5d4883dd74c647
carlos josue lopez  castillo <carlosjlc0216@gmail.com>
2024-03-18 23:54:34 -0600

    add evidencias page

--- app/admin/evidencias/page.js
+++ app/admin/evidencias/page.js
@@ -1,12 +1,64 @@
-import React from 'react'
+"use client";
+import { useUser } from "@/app/context/useContext";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const Evidencias = () => {
-  return (
-    <div className='w-full flex justify-center items-center p-5'> 
-        <h1 className='text-3xl font-black '>Aquí se muestran todas las evidencias almacenadas</h1>
-        
-    </div>
-  )
-}
+  const { getFiles, archivos } = useUser(); // Ajusta esto según tu contexto
+  const [show, setShow] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-export default Evidencias
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        if (!archivos) {
+          await getFiles();
+        }
+      } catch (error) {
+        console.error("Error al obtener datos:", error);
+      } finally {
+        setLoading(false);
+        setShow(true);
+      }
+    };
+
+    fetchData();
+    console.log(archivos);
+  }, [archivos, getFiles]);
+  return (
+    <div className="w-full  justify-center items-center p-5">
+      <h1 className="text-3xl font-black ">
+        Aquí se muestran todas las evidencias almacenadas
+      </h1>
+      <div>
+        {loading ? (
+          <div>cargando...</div>
+        ) : (
+          show &&
+          Array.isArray(archivos) && (
+            <div className="container mx-auto py-8">
+            <h1 className="text-3xl font-bold mb-4">Archivos:</h1>
+            <div className="grid grid-cols-1 gap-4">
+              {archivos.map(archivo => (
+                <div key={archivo.id} className="bg-gray-100 p-4 rounded-lg shadow-md">
+                  <Link className="text-lg font-semibold hover:cursor-pointer hover:text-blue-500" target="_blank" href={`http://localhost:4000/uploads/${archivo.originalname}`}>
+                    {archivo.filename}
+                  </Link>
+                  <p>Originalname: {archivo.originalname}</p>
+                  <p>Mimetype: {archivo.mimetype}</p>
+                  <p>Size: {archivo.size}</p>
+                  <p>ID Usuario: {archivo.idUsuario}</p>
+                  <p>ID Pregunta: {archivo.idPregunta}</p>
+                  <p>Created at: {archivo.created_at}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Evidencias;
--- app/context/useContext.js
+++ app/context/useContext.js
@@ -7,6 +7,7 @@ export const UserContext = createContext();
 
 export default function UserProvider({ children }) {
   const [preguntas, setPreguntas] = useState({});
+  const [archivos, setArchivos] = useState({})
   const [dataUser, setDataUser] = useState({})
   const [user, setUser] = useState(() => {
     const storedUserData = localStorage?.getItem('userData');
@@ -17,13 +18,19 @@ export default function UserProvider({ children }) {
     const { data } = await axios("http://localhost:4000/api/preguntas/viewAll");
     setDataUser(data)
   };
+  const getFiles = async () => {
+    const { data } = await axios("http://localhost:4000/api/files/getFiles");
+    setArchivos(data)
+    
+  }
   useEffect(() => {
     getPreguntas();
-    console.log(preguntas)
+    getFiles()
+    console.log(archivos)
   }, []);
 
   return (
-    <UserContext.Provider value={{ user,setUser, preguntas,getPreguntas,dataUser }}>
+    <UserContext.Provider value={{ user,setUser, preguntas,getPreguntas,dataUser,archivos,getFiles }}>
       {children}
     </UserContext.Provider>
   );
