a88ad817c0243e489220c39faa5b9e7f824b39cd
ISC-2 <carlosjosue0216>
2024-04-16 11:23:32 -0600

    update results

--- app/page.js
+++ app/page.js
@@ -91,7 +91,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-gray-100 p-24">
       <h1 className="text-5xl font-black p-5 my-5">
-        Criterios e indicadores para programas de ingenieria
+      Criterios e indicadores para programas de ingeniería.
       </h1>
       <div className="p-5 bg-white rounded-lg shadow-md my-5 ">
         <div className="p-5">
@@ -150,58 +150,7 @@ export default function Home() {
                 )}
               </div>
             ) : (
-              <div key={criterio.id} className="p-2">
-                <h1
-                  className="font-bold text-2xl hover:cursor-pointer flex items-center gap-10"
-                  onClick={() => handleCriterio(criterio.id)}
-                >
-                  {criterio.title}
-                  <svg
-                    className={`w-6 h-6 text-gray-800 dark:text-white ${
-                      criteriosState[criterio.id] ? "transform rotate-180" : ""
-                    }`}
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="m8 7 4 4 4-4m-8 6 4 4 4-4"
-                    />
-                  </svg>
-                </h1>
-                {criteriosState[criterio.id] && (
-                  <div>
-                    {criterio.subcriterios.map((subcriterio) => (
-                      <div key={subcriterio.id} className="p-2">
-                        <Link
-                          className="font-semibold ms-3 justify-between flex"
-                          href={subcriterio.path}
-                        >
-                          {subcriterio.title}
-                          <svg
-                            className="w-6 h-6 text-gray-800 dark:text-white mx-3"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M3 4a1 1 0 0 0-.8 1.6L6.6 12l-4.4 6.4A1 1 0 0 0 3 20h13.2c.3 0 .6-.2.8-.4l4.8-7a1 1 0 0 0 0-1.2l-4.8-7a1 1 0 0 0-.8-.4H3Z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </Link>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
+             ""
             )
           )}
         </div>
