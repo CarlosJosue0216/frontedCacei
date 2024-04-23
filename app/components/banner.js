115c21f5dd33aa1f0bc4e646d1a176afd94d1320
CarlosJosue0216 <calolosjlc0216@gmail.com>
2024-04-22 23:45:43 -0600

    Merge branch 'main' of https://github.com/CarlosJosue0216/frontedCacei

--- app/components/banner.js
+++ app/components/banner.js
@@ -3,7 +3,7 @@ const Banner = ({alerta}) => {
   console.log(alerta)
   return (
     <div>
-        <h1 className={`${alerta?.typeAlert ? 'bg-green-400' : 'bg-red-500'} p-3 text-center rounded-lg font-bold text-xl text-white capitalize`}> {alerta?.msg} </h1>
+        <h1 className={`${alerta.typeAlert ? 'bg-green-400' : 'bg-red-500'} p-3 text-center rounded-lg font-bold text-xl text-white capitalize`}> {alerta.msg} </h1>
     </div>
   )
 }
